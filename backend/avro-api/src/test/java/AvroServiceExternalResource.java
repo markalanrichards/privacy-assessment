20d8769f839e2478e284239750e0ef1409c574e3
Mark Richards <mark.alan.richards@gmail.com>
2022-01-08 16:30:54 +0000

    Swapping to http in api testing

--- backend/avro-api/src/test/java/AvroServiceExternalResource.java
+++ backend/avro-api/src/test/java/AvroServiceExternalResource.java
@@ -1,12 +1,15 @@
-import java.io.IOException;
-import java.net.InetSocketAddress;
+import java.net.URI;
 import java.util.Random;
 import java.util.UUID;
-import org.apache.avro.ipc.Server;
-import org.apache.avro.ipc.netty.NettyServer;
-import org.apache.avro.ipc.netty.NettyTransceiver;
+
+import org.apache.avro.ipc.HttpTransceiver;
+import org.apache.avro.ipc.ResponderServlet;
 import org.apache.avro.ipc.specific.SpecificRequestor;
 import org.apache.avro.ipc.specific.SpecificResponder;
+import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.server.handler.ErrorHandler;
+import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -14,54 +17,83 @@ import org.mockito.ArgumentMatcher;
 import org.mockito.Mockito;
 
 public class AvroServiceExternalResource<T> implements BeforeEachCallback, AfterEachCallback {
-  public static String RANDOM_UTF8() {
-    return UUID.randomUUID().toString();
-  }
 
-  public static Random RANDOM = new Random();
+    private HttpTransceiver httpTransceiver;
 
-  public static ArgumentMatcher<CharSequence> CHARSEQUENCE_MATCHER(final CharSequence toMatch) {
+    public static String RANDOM_UTF8() {
+        return UUID.randomUUID().toString();
+    }
 
-    return new ArgumentMatcher<CharSequence>() {
-      @Override
-      public boolean matches(CharSequence o) {
-        return o.toString().equals(toMatch.toString());
-      }
-    };
-  }
+    public static Random RANDOM = new Random();
 
-  private Server server;
-  private NettyTransceiver nettyTransceiver;
-  private T service;
-  private final Class<T> clazz;
-  private T client;
+    public static ArgumentMatcher<CharSequence> CHARSEQUENCE_MATCHER(final CharSequence toMatch) {
 
-  public AvroServiceExternalResource(Class<T> clazz) {
-    this.clazz = clazz;
-  }
+        return new ArgumentMatcher<CharSequence>() {
+            @Override
+            public boolean matches(CharSequence o) {
+                return o.toString().equals(toMatch.toString());
+            }
+        };
+    }
 
-  @Override
-  public void beforeEach(ExtensionContext context) throws IOException, InterruptedException {
-    service = Mockito.mock(clazz);
-    server = new NettyServer(new SpecificResponder(clazz, service), new InetSocketAddress(0));
+    private Server server;
+    private T service;
+    private final Class<T> clazz;
+    private T client;
 
-    server.start();
+    public AvroServiceExternalResource(Class<T> clazz) {
+        this.clazz = clazz;
+    }
 
-    nettyTransceiver = new NettyTransceiver(new InetSocketAddress(server.getPort()));
-    client = SpecificRequestor.getClient(clazz, nettyTransceiver);
-  }
+    @Override
+    public void beforeEach(ExtensionContext context) throws Exception {
 
-  @Override
-  public void afterEach(ExtensionContext context) {
-    nettyTransceiver.close();
-    server.close();
-  }
+        service = Mockito.mock(clazz);
 
-  public T getClient() {
-    return client;
-  }
+        final ServletHandler handler = new ServletHandler();
+        server = new Server();
+        final ServerConnector serverConnector = new ServerConnector(server);
+        serverConnector.setHost("127.0.0.1");
+        serverConnector.setPort(0);
+        server.setConnectors(new Connector[]{serverConnector});
+        server.setHandler(handler);
+        Slf4jRequestLogWriter slf4jRequestLogWriter = new Slf4jRequestLogWriter();
 
-  public T getService() {
-    return service;
-  }
+        final CustomRequestLog customRequestLog =
+                new CustomRequestLog(slf4jRequestLogWriter, CustomRequestLog.EXTENDED_NCSA_FORMAT);
+
+        server.setRequestLog(customRequestLog);
+        ErrorHandler errorHandler = new ErrorHandler();
+        errorHandler.setShowMessageInTitle(false);
+        errorHandler.setShowStacks(false);
+        server.setErrorHandler(errorHandler);
+
+        SpecificResponder responder = new SpecificResponder(clazz, service);
+        ResponderServlet responderServlet = new ResponderServlet(responder);
+        handler.addServletWithMapping(new ServletHolder(responderServlet), "/avpr");
+
+        server.start();
+        final ServerConnector connector = (ServerConnector) server.getConnectors()[0];
+
+        final int port = connector.getLocalPort();
+        URI uri = URI.create("http://127.0.0.1:" + port + "/avpr");
+
+        httpTransceiver = new HttpTransceiver(uri.toURL());
+
+        client = SpecificRequestor.getClient(clazz, httpTransceiver);
+    }
+
+    @Override
+    public void afterEach(ExtensionContext context) throws Exception {
+        httpTransceiver.close();
+        server.stop();
+    }
+
+    public T getClient() {
+        return client;
+    }
+
+    public T getService() {
+        return service;
+    }
 }
